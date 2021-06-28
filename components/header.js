7fc2b6db4d1aca07c22bac66200d61aa17f4b01f
Badatnames16 <56033125+Badatnames16@users.noreply.github.com>
2021-06-27 21:45:20 -0400

    Error fix

--- components/header.js
+++ components/header.js
@@ -8,9 +8,9 @@ class Header extends HTMLElement {
         <header>
             <a href="index.html" class="btn btn-secondary" id="introBtn">Home</a>
             <a href="../pages/personalProjects.html" class="btn btn-secondary" id="introBtn">Personal Projects</a>
-            <a href="resume.html" class="btn btn-secondary" id="introBtn">Resume</a>
-            <a href="blog.html" class="btn btn-secondary" id="introBtn">Blog</a>
-            <a href="contact.html" class="btn btn-secondary" id="introBtn">Contact</a>
+            <a href="../pages/resume.html" class="btn btn-secondary" id="introBtn">Resume</a>
+            <a href="../pages/blog.html" class="btn btn-secondary" id="introBtn">Blog</a>
+            <a href="../pages/contact.html" class="btn btn-secondary" id="introBtn">Contact</a>
             <hr />
         </header>
       `;
