6765c4b68ed63d9d50eb6999af72cd32bae17eb9
Badatnames16 <56033125+Badatnames16@users.noreply.github.com>
2021-06-27 21:42:33 -0400

    Added seperate pages and header

---
+++ components/header.js
@@ -0,0 +1,20 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <header>
+            <a href="index.html" class="btn btn-secondary" id="introBtn">Home</a>
+            <a href="personalProjects.html" class="btn btn-secondary" id="introBtn">Personal Projects</a>
+            <a href="resume.html" class="btn btn-secondary" id="introBtn">Resume</a>
+            <a href="blog.html" class="btn btn-secondary" id="introBtn">Blog</a>
+            <a href="contact.html" class="btn btn-secondary" id="introBtn">Contact</a>
+            <hr />
+        </header>
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
